3ddf1effe38a1778e557421a3bc11c19a7333a5e
xorsal <zorzal@defi.sucks>
2025-03-17 18:04:13 -0400

    chore: remove escrow (#58)
    I'm removing escrow and clawback contracts 
    We migrated them to defi-wonderland/aztec-extensions

--- src/ts/test/utils.ts
+++ src/ts/test/utils.ts
@@ -5,16 +5,10 @@ import {
   AztecAddress,
   UniqueNote,
   AccountWallet,
-  PXE,
-  Wallet,
   createPXEClient,
-  AccountWalletWithSecretKey,
   FieldLike,
 } from '@aztec/aztec.js';
-import { computePartialAddress, deriveKeys } from '@aztec/circuits.js';
 import { TokenContract } from '../../artifacts/Token.js';
-import { EscrowContract } from '../../artifacts/Escrow.js';
-import { ClawbackEscrowContract } from '../../artifacts/ClawbackEscrow.js';
 
 export const logger = createLogger('aztec:aztec-standards');
 
@@ -50,18 +44,6 @@ export const expectAccountNote = (note: UniqueNote, owner: AztecAddress, secret?
   }
 };
 
-export const expectClawbackNote = (
-  note: UniqueNote,
-  sender: AztecAddress,
-  receiver: AztecAddress,
-  escrow: AztecAddress,
-) => {
-  // expect(note.note.items.length).toBe(3);
-  expect(note.note.items[0]).toEqual(new Fr(sender.toBigInt()));
-  expect(note.note.items[1]).toEqual(new Fr(receiver.toBigInt()));
-  expect(note.note.items[2]).toEqual(new Fr(escrow.toBigInt()));
-};
-
 export const expectTokenBalances = async (
   token: TokenContract,
   address: AztecAddress,
@@ -77,38 +59,3 @@ export const expectTokenBalances = async (
 
 export const AMOUNT = 1000n;
 export const wad = (n: number = 1) => AMOUNT * BigInt(n);
-
-export async function deployEscrow(pxes: PXE[], deployerWallet: Wallet, owner: AztecAddress): Promise<EscrowContract> {
-  const escrowSecretKey = Fr.random();
-  const escrowPublicKeys = (await deriveKeys(escrowSecretKey)).publicKeys;
-  const escrowDeployment = EscrowContract.deployWithPublicKeys(
-    escrowPublicKeys,
-    deployerWallet,
-    owner,
-    escrowSecretKey,
-  );
-  const escrowInstance = await escrowDeployment.getInstance();
-
-  await pxes[0].registerAccount(escrowSecretKey, await computePartialAddress(escrowInstance));
-  // TODO: instead of register it here for Bob, we should use the Escrow::PrivacyKeys event (or something else!)
-  await pxes[1].registerAccount(escrowSecretKey, await computePartialAddress(escrowInstance));
-
-  // TODO: Deployment must happen after Escrow keys are registered, otherwise e2e will fail due being unable to retrieve pub keys
-  const tx = await escrowDeployment.send().wait();
-  const escrowContract = await EscrowContract.at(escrowInstance.address, deployerWallet);
-
-  const contractMetadata = await pxes[0].getContractMetadata(escrowInstance.address);
-  expect(contractMetadata.isContractPubliclyDeployed).toBeTruthy();
-
-  logger.info('escrow deployed', escrowContract.address);
-  return escrowContract;
-}
-
-export async function deployClawbackEscrow(deployerWallet: AccountWalletWithSecretKey) {
-  const clawbackDeployment = ClawbackEscrowContract.deploy(deployerWallet);
-  const tx = await clawbackDeployment.send().wait();
-  const clawbackContract = tx.contract;
-
-  logger.info(`clawback address: ${clawbackContract.address}`);
-  return clawbackContract;
-}
